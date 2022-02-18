818e15561ba028b58df06cdb1d9f70ccd6407d4d
Paras Subedi <subediparas5@gmail.com>
2022-02-18 17:46:15 +0545

    update comment reply module

--- routes/productCommentReply.js
+++ routes/productCommentReply.js
@@ -65,4 +65,103 @@ router.post('/reply/:comment_id/add', verify, async (request, response) => {
 
 });
 
+
+router.put('/reply/update/:reply_id', verify, async (request, response) => {
+    if (!mongoose.Types.ObjectId.isValid(request.params.reply_id)) {
+        return response.status(400).send({
+            message: "No reply found with this id",
+            data: {}
+        });
+    }
+
+    await ProductCommentReply.findOne({ _id: request.params.reply_id })
+        .then(async (reply) => {
+            if (!reply) {
+                response.send({
+                    message: "No Reply Found",
+                    data: {}
+                });
+            }
+            else {
+                if (reply.user_id != request.user._id) {
+                    return response.status(400).send({
+                        message: "Access Denied",
+                        data: {}
+                    });
+                }
+                else {
+                    try {
+                        const { error } = commentReplyValidation(request.body);
+                        if (error) return response.status(400).send({
+                            message: error.details[0].message,
+                            data: error.details[0]
+                        });
+
+                        await ProductCommentReply.updateOne({ _id: reply._id }, {
+                            reply: request.body.reply,
+                        });
+
+                        let queryList = [
+                            {
+                                $lookup: {
+                                    from: 'users',
+                                    localField: 'user_id',
+                                    foreignField: '_id',
+                                    as: 'user'
+                                }
+                            },
+                            {
+                                $unwind: '$user'
+                            },
+                            {
+                                $lookup: {
+                                    from: 'productcomments',
+                                    localField: 'comment_id',
+                                    foreignField: '_id',
+                                    as: 'comment'
+                                }
+                            },
+                            {
+                                $unwind: '$comment'
+                            },
+                            {
+                                $match: {
+                                    "_id": mongoose.Types.ObjectId(request.params.reply_id)
+                                }
+                            },
+                            {
+                                $project: {
+                                    '_id': 1,
+                                    'comment_id': 1,
+                                    'reply': 1,
+                                    'updatedAt': 1,
+                                    'user._id': 1,
+                                    'user.name': 1,
+                                    'comment._id': 1,
+                                    'comment.product_id': 1,
+                                    'comment.updatedAt': 1,
+                                    'comment.comment': 1,
+                                    'comment.user_id': 1,
+                                }
+                            }
+                        ];
+
+                        let updatedReply = await ProductCommentReply.aggregate(queryList);
+                        response.send({
+                            message: "Reply edited successfully",
+                            comment: ProductCommentReply.hydrate(updatedReply[0])
+                        });
+
+                    }
+                    catch (err) {
+                        return response.status(400).send({
+                            message: err.message || "Error occured",
+                            data: err
+                        });
+                    }
+                }
+            }
+        })
+})
+
 module.exports = router;
