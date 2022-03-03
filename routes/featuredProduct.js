d1656889064a86d694fd401448bd623517199e56
Prashant Subedi <psubedi@Prashants-MacBook-Air.local>
2022-03-03 10:06:04 +0545

    mac backup

--- routes/featuredProduct.js
+++ routes/featuredProduct.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const verify = require('../verifyToken');
 const FeaturedProduct = require('../models/FeaturedProduct')
 const Admin = require('../models/Admin')
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
 
 router.post('/:product_id', verify, async (request, response) => {
     let admin_check = await Admin.findOne({ user_id: request.user._id })
@@ -33,7 +35,7 @@ router.post('/:product_id', verify, async (request, response) => {
                 if (db_date <= Date.now()) {
                     date = new Date()
                     data = await FeaturedProduct.updateOne(
-                        { product_id: request.params.product_id },
+                        { product_id: product_check.product_id },
                         {
                             expiryDate: date.setDate(date.getDate() + 30),
                             featured_by: admin_check.user_id
@@ -44,7 +46,7 @@ router.post('/:product_id', verify, async (request, response) => {
                     //not expired
                     date = new Date(db_date);
                     data = await FeaturedProduct.updateOne(
-                        { product_id: request.params.product_id },
+                        { product_id: product_check.product_id },
                         {
                             expiryDate: date.setDate(date.getDate() + 30),
                             featured_by: admin_check.user_id
@@ -53,13 +55,24 @@ router.post('/:product_id', verify, async (request, response) => {
                 }
             }
             else {
-                date = new Date();
-                const featuredProduct = new FeaturedProduct({
-                    product_id: request.params.product_id,
-                    expiryDate: date.setDate(date.getDate() + 30),
-                    featured_by: admin_check.user_id
-                });
-                data = await featuredProduct.save();
+                let product = await Product.findOne({ _id: request.params.product_id })
+                if (product) {
+                    console.log(product._id);
+                    date = new Date();
+                    const featuredProduct = new FeaturedProduct({
+                        product_id: mongoose.ObjectId(product._id),
+                        expiryDate: date.setDate(date.getDate() + 30),
+                        featured_by: admin_check.user_id
+                    });
+                    data = await featuredProduct.save();
+                }
+                else {
+                    return response.send({
+                        message: "Product not found",
+                        data: {}
+                    });
+                }
+
             }
             return response.send({
                 message: "Product featured successfully",
@@ -79,8 +92,68 @@ router.post('/:product_id', verify, async (request, response) => {
 });
 
 
-router.get('/', verify, async (request, response) => {
+router.get('/all', verify, async (request, response) => {
+    try {
+        let queryList = [
+            {
+                $lookup: {
+                    from: 'products',
+                    localField: 'product_id',
+                    foreignField: '_id',
+                    as: 'product'
+                }
+            },
+            {
+                $unwind: '$product'
+            },
+        ]
+        if (request.query.product_id) {
+            queryList.push({
+                $match: {
+                    product_id: mongoose.Types.ObjectId(request.query.product_id)
+                },
+            })
+        }
+        queryList.push({
+            $project: {
+                '_id': 1,
+                "product.product_name": 1,
+                'product.description': 1,
+                'product.image': 1,
+                'product.price': 1,
+                'product.availability': 1,
+                'product.address': 1,
+                'product.condition': 1,
+                'product.tags': 1,
+                "product.negotiation": 1,
+                'product.delivery': 1,
+                'product.usedFor': 1,
+                'product.createdAt': 1,
+                'product.owner.name': 1,
+                'product.owner.email': 1,
+                'product.owner._id': 1,
+                // 'category_details._id': 1,
+                // 'category_details.slug': 1,
+                // 'category_details.sub_slug': 1,
+                // 'category_details.name': 1,
+                'comments': -1
+            }
+        });
 
- })
+        FeaturedProduct.aggregate(queryList)
+            .then(product => {
+                return response.send({
+                    message: "Data retrived",
+                    data: product.map(doc => FeaturedProduct.hydrate(doc)),
+                })
+            });
+    }
+    catch (err) {
+        response.status(400).send({
+            message: err.message || "Error occured while retriving products data",
+            data: err
+        });
+    }
+})
 
 module.exports = router;
